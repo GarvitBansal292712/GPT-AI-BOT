18ca69e14ac5de6b07a1564866669b6b0b6fe93e
GARVIT BANSAL 292712 <118173085+GarvitBansal292712@users.noreply.github.com>
2024-04-15 23:58:04 +0530

    Landing page started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
+    <Home/>
       <div>
         Hey I am a chat bot! Using Open AI Api to generate responses.
       </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full h-[100vh] bg-gradient-to-br from-blue-100 via-purple-200 to-yellow-200 '>HEADER</div>
+  )
+}
+
+export default Home
