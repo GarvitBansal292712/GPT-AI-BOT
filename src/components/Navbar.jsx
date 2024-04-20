fc5f9d8ec54eefd5b54a32f8ae6868ee26f93263
GARVIT BANSAL 292712 <118173085+GarvitBansal292712@users.noreply.github.com>
2024-04-20 23:53:14 +0530

    Chat page tweaking done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,25 @@ import logo from "../assets/images/logo.png"
 const Navbar = () => {
   return (
     <>
+        {/* NAVBAR */}
+        <div className="flex justify-center font-mono ">
 
+        <div className="flex justify-center fixed top-2">
+          <div className=" text-black  flex justify-around  p-2  bg-[#ffffff] gap-5 rounded-2xl">{/* bg-[#ff */}{/* ffff23] */}
+            <div className=" flex gap-2 font-bold">
+              <img src={logo} alt="" width={"25px"} className="logo invert" />
+              NexSphere
+            </div>
+            <div>
+              <ul className="flex gap-4 ">
+                <li>Home</li>
+                <li>Chat</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        </div>
+        {/* NAVBAR */}
     </>
   );
 };
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,29 +1,14 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import "../index.css";
+import { IoSend } from "react-icons/io5";
+// import { Suspense } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls } from "@react-three/drei";
 // import Nex from "/Users/garvi/Projects/AI API CHAT BOT PROJECT/public/Nex"
 const Chat = () => {
   return (
     <>
-      {/* NAVBAR */}
-      <div className="flex justify-center my-2 font-mono">
-        <div className=" text-black  flex justify-around  p-2  bg-[#ffffff23] gap-5 rounded-2xl">
-          <div className=" flex gap-2 font-bold">
-            <img src={logo} alt="" width={"25px"} className="logo" />
-            NexSphere
-          </div>
-          <div>
-            <ul className="flex gap-4 ">
-              <li>Home</li>
-              <li>Chat</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      {/* NAVBAR */}
       {/* 3d model */}
       {/* <Canvas>
         <Suspense fallback={null}>
@@ -33,42 +18,52 @@ const Chat = () => {
       {/* 3d model */}
       {/* Chat */}
       <div className="flex justify-center items-center flex-col text-white font-mono  mt-[200px]">
-            <img src={logo} alt="" width={"100px"} className="logo" />
+        <img src={logo} alt="" width={"100px"} className="logo mb-[50px] -z-10" />
+        <div className="flex justify-center flex-col items-center mb-[90px] -z-10">
+          {/* CHAT IS GOING TO START FROM HERE AI CHAT */}
+          <div className="bg-[#FFFFFF0A] flex flex-col p-3 rounded-xl m-5 border-[1px] border-[#FFFFFF14] w-[70%] ">
+            <div className="flex gap-3 items-center">
+              <img src={logo} alt="" width={"25px"} className="" />
+              <p>NexSphere</p>
+            </div>
+            <div>
+              <p className="ml-[36px]">
+                Hey! I am NexSphere, your AI assistant. How can I help you
+                today?
+              </p>
+            </div>
+          </div>
+          {/* CHAT IS GOING TO START FROM HERE HUMAN CHAT */}
+          <div className="bg-[#ffffffc5] text-black flex flex-col p-3 rounded-xl mx-5 mt-5 mb-2 border-[1px] border-[#FFFFFF14] w-[70%] ">
+            <div className="flex gap-3 items-center">
+              <img src={logo} alt="" width={"25px"} className="" />
+              <p>You</p>
+            </div>
+            <div>
+              <p className="ml-[36px]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptates reprehenderit, nulla autem deserunt beatae velit
+                earum eius cupiditate non voluptatem.
+              </p>
+            </div>
+          </div>
 
-            <h1 className="text-center m-2">
-            Hey! I am NexSphere, your AI assistant. How can I help you today?
-            </h1>
-            {/* CHAT IS GOING TO START FROM HERE AI CHAT */}
-            <div className="bg-[#FFFFFF0A] flex flex-col p-3 rounded-xl m-5 border-[1px] border-[#FFFFFF14] w-[90%]">
-                <div className="flex gap-3 items-center">
-                    <img src={logo} alt="" width={"25px"} className="" />
-                    <p>NexSphere</p>
-                </div>
-                <div>
-                    <p className="ml-[36px]">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptates reprehenderit.
-                    </p>
-                </div>
-            </div>
-            {/* CHAT IS GOING TO START FROM HERE HUMAN CHAT */}
-            <div className="bg-[#ffffffc5] text-black flex flex-col p-3 rounded-xl mx-5 mt-5 mb-2 border-[1px] border-[#FFFFFF14] w-[90%]">
-                <div className="flex gap-3 items-center">
-                    <img src={logo} alt="" width={"25px"} className="" />
-                    <p>You</p>
-                </div>
-                <div>
-                    <p className="ml-[36px]">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptates reprehenderit, nulla autem deserunt beatae velit earum
-                    eius cupiditate non voluptatem.
-                    </p>
-                </div>
-            </div>
-            {/* Input  */}
-            <div className="w-full">
-                <input type="text" name="" id="" className="flex bg-[#FFFFFF0A] p-4 rounded-xl relative bottom-0 justify-center w-[90%] items-center" />
-            </div>
+
+        </div>
+
+        {/* Input  */}
+        <div className="w-full flex justify-center mt-14  items-center fixed bottom-[2px] z-10">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Message NexSphere..."
+            className="flex bg-[#000712] border-[2px] border-white p-4 rounded-xl  justify-center w-[91%] items-center"
+          />
+          <button type="submit" className="absolute right-[100px] ">
+            <IoSend />
+          </button>
+        </div>
       </div>
       {/* Chat */}
     </>
